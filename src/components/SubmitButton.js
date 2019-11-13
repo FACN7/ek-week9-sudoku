4e357db5d2a5bf1a2a045018130ad5d6d5aaa232
ebraheemabbas <ebraheemabbas51@gmail.com>
2019-11-13 14:17:02 +0200

    check if won

--- src/components/SubmitButton.js
+++ src/components/SubmitButton.js
@@ -1,22 +1,19 @@
 import React from "react";
+import { useAlert } from "react-alert";
 
+const SubmitButton = ({onclick,board }) => {
+  const alert = useAlert();
 
-const SubmitButton = (props) => {
-    let {board = null ,onclick} = props;
-    
-    // let clickfunction = () => console.log("just clicked");
-     if (!board) {
-        console.log("no board recieved");
-        // onclick = console.log("Enter a board please")
-    }
-    // else {
-    //     console.table(board);
-    //     clickfunction = () => console.log("We have board CHANGE THIS FUNCTION!!!");
-    // }
-    
-    return (
-        <div><button onClick={onclick}>Check</button></div>
-    );
+  // let clickfunction = () => console.log("just clicked");
+  if (!board) {
+    console.log("no board recieved");
+    // onclick = console.log("Enter a board please")
+  }
 
-}
+  return (
+    <div>
+      <button onClick={()=>{onclick(board, alert)}}>Check</button>
+    </div>
+  );
+};
 export default SubmitButton;
